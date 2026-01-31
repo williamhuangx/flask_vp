5cb1a1c59480464ac05df5bd585aa9ff3f62504e
williamhuangv <13860669931@qq.com>
2026-01-31 19:59:45 +0800

    Create simple Flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, Flask!'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
